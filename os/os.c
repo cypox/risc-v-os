9cb05cdb06698abc5617ab6c763f37623de02107
cypox <am_neggaz@esi.dz>
2022-03-10 21:29:32 +0100

    added libs

--- os/os.c
+++ os/os.c
@@ -1,22 +1,23 @@
-#include <stdint.h>
+#include "os.h"
 
-#define UART        0x10000000
-#define UART_THR    (uint8_t*)(UART+0x00) // THR:transmitter holding register
-#define UART_LSR    (uint8_t*)(UART+0x05) // LSR:line status register
-#define UART_LSR_EMPTY_MASK 0x40          // LSR Bit 6: Transmitter empty; both the THR and LSR are empty
+#define STACK_SIZE 1024
+reg_t task0_stack[STACK_SIZE];
+struct context ctx_os;
+struct context ctx_task;
 
-int lib_putc(char ch) {
-	while ((*UART_LSR & UART_LSR_EMPTY_MASK) == 0);
-	return *UART_THR = ch;
-}
+extern void sys_switch();
 
-void lib_puts(char *s) {
-	while (*s) lib_putc(*s++);
+void user_task0(void)
+{
+	lib_puts("Task0: Context Switch Success !\n");
+	while (1) {} // stop here.
 }
 
 int os_main(void)
 {
-	lib_puts("Hello OS!\n");
-	while (1) {}
+	lib_puts("rvCOS start\n");
+	ctx_task.ra = (reg_t) user_task0;
+	ctx_task.sp = (reg_t) &task0_stack[STACK_SIZE-1];
+	sys_switch(&ctx_os, &ctx_task);
 	return 0;
 }
